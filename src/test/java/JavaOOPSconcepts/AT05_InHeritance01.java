f4c62439537717cc67d0d8803015933e2d1f659d
Jagan Etikala <Jagan.Etikala@tabcorp.com.au>
2022-05-06 10:24:51 +1000

    NewChanges

---
+++ src/test/java/JavaOOPSconcepts/AT05_InHeritance01.java
@@ -0,0 +1,25 @@
+package JavaOOPSconcepts;
+
+import org.junit.Test;
+// lets imagine this is parent class
+public class AT05_InHeritance01 {
+
+
+    @Test
+    public void inhMethod01(){
+        System.out.println("This is inhMethod01");
+    }
+
+    @Test
+    public void inhMethod02(){
+        System.out.println("This is inhMethod02");
+    }
+    @Test
+    public void inhMethod03(){
+        System.out.println("This is inhMethod03");
+    }
+
+
+
+
+}
